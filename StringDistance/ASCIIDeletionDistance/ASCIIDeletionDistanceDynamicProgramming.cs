8ebb08cc4e34e92fb5e0065a8f0d10d195e4b3e0
Frederic Torres <ftorres@emoneyadvisor.com>
2018-11-17 18:04:42 -0500

    Implement ASCII deletion distance using dynamic programming

---
+++ StringDistance/ASCIIDeletionDistance/ASCIIDeletionDistanceDynamicProgramming.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DynamicSugar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringDistances
+{
+    /*
+      ASCII Deletion Distanc eDynamic Programming
+      ===========================================
+      Based on the Fisher Wagner algo but simpler we can solve the problem
+      using dynamic programming
+      1. Create a matrix for the 2 word
+      2. For each row check if the row letter is in one of the column if yes mark the column with a 1
+      3. For each row if all column contain a 0 the letter was not used we must delete it
+      4. For each col if all row contain a 0 the letter was not used we must delete it
+
+        b r e a d
+    g   0 0 0 0 0 delete g
+    r   0 1 0 0 0 keep r
+    e   0 0 1 0 0 keep e
+    d   0 0 0 0 1 keep d
+    
+        delelte b, keep r, keep e delete a, keep d
+        delete char g, b, a
+
+        c a t
+    b   0 0 0 delete b
+    a   0 1 0 keep a
+    t   0 0 1 keep t
+        delete c
+        delete chars b,c             
+
+        b a t
+    c   0 0 0
+    a   0 1 0
+    t   0 0 1
+    a   0 0 0
+
+        delete c, delete second a    
+
+*/
+
+    public class ASCIIDeletionDistanceDynamicProgramming
+    {
+
+        /// <summary>
+        /// Store all the letter to delete in str1 and str2
+        /// </summary>
+        List<int> _deletions = new List<int>();
+
+        private bool IsRowAll0(int[,] matrix, int row, int maxCol)
+        {
+            var r = true;
+            for(var j=0; j < maxCol; j++) 
+                if(matrix[row, j] != 0)
+                    r = false;
+            return r;
+        }
+        private bool IsColAll0(int[,] matrix, int col, int maxRow)
+        {
+            var r = true;
+            for(var i=0; i < maxRow; i++) 
+                if(matrix[i, col] != 0)
+                    r = false;
+            return r;
+        }
+
+        public FisherWagner.FisherWagnerResult Compute(string word1, string word2)
+        {
+            int len1 = word1.Length;
+	        int len2 = word2.Length;
+ 
+            int[,] dp = new int[len1, len2]; // Will be filled with 0
+
+	        for (int i = 0; i < len1; i++)
+            {
+		        char c1 = word1[i];
+		        for (int j = 0; j < len2; j++) {
+			        char c2 = word2[j];
+                    if( c1 == c2)
+                    {
+                        if(IsColAll0(dp, j, len1))
+                            dp[i, j] += 1;
+                    }
+		        }
+	        }
+
+            for (int i = 0; i < len1; i++)
+            {
+		        char c = word1[i];
+                if(IsRowAll0(dp, i, len2))
+                    _deletions.Add(c);
+	        }
+
+            for (int j = 0; j < len2; j++)
+            {
+		        char c = word2[j];
+                if(IsColAll0(dp, j, len1))
+                    _deletions.Add(c);
+	        }
+
+            return new FisherWagner.FisherWagnerResult ()
+            {
+                Value = this.GetDeletionsCount(),
+                Matrix = dp
+            };
+        }        
+
+        private int GetDeletionsCount()
+        {
+            var total = 0; // Just count the number of deletions using the ascii value
+            this._deletions.ForEach(c => {
+                total += c;
+            });
+            return total;
+        }
+    }
+
+}
--- StringDistance/UnitTests/ASCIIDeletionDistanceUnitTest.cs
+++ StringDistance/UnitTests/ASCIIDeletionDistanceUnitTest.cs
@@ -10,6 +10,19 @@ namespace StringDistances
     [TestClass]
     public class ComputeASCIIDeletionDistanceUnitTest
     {
+
+
+        [TestMethod]
+        public void __DynamicProgramming_ComputeDistance_SimpleCases()
+        {
+            Assert.AreEqual(197, new ASCIIDeletionDistanceDynamicProgramming().Compute("cat", "bat").Value);
+            Assert.AreEqual(294, new ASCIIDeletionDistanceDynamicProgramming().Compute("cata", "bat").Value);
+            Assert.AreEqual(319, new ASCIIDeletionDistanceDynamicProgramming().Compute("cat", "batz").Value);
+            Assert.AreEqual( 99, new ASCIIDeletionDistanceDynamicProgramming().Compute("at", "cat").Value);
+            Assert.AreEqual(559, new ASCIIDeletionDistanceDynamicProgramming().Compute("thought", "slough").Value);
+            Assert.AreEqual(298, new ASCIIDeletionDistanceDynamicProgramming().Compute("bread", "gred").Value);
+        }
+
         [TestMethod]
         public void ComputeDistance_SimpleCases()
         {
@@ -19,6 +32,26 @@ namespace StringDistances
             Assert.AreEqual( 99, new ASCIIDeletionDistance().Compute("at", "cat").Value);
             Assert.AreEqual(559, new ASCIIDeletionDistance().Compute("thought", "slough").Value);
             Assert.AreEqual(298, new ASCIIDeletionDistance().Compute("bread", "gred").Value);
+
+/*
+        b r e a d
+    g   0 0 0 0 0 delete g
+    r   0 1 0 0 0 keep r
+    e   0 0 1 0 0 keep e
+    d   0 0 0 0 1 keep d
+        
+        delelte b, keep r, keep e delete a, keep d
+
+        g, b, a
+
+        c a t 
+    b   0 0 0
+    a   0 1 0
+    t   0 0 1
+             
+*/
+
+
         }
 
         [TestMethod]
@@ -39,6 +72,19 @@ in str1 at -1 char 't'
         public void ComputeDistance_MultipleRepeatOfSameLetter_InStr2()
         {
             var result = new ASCIIDeletionDistance().Compute("cat", "batzz");
+
+
+/*
+
+        c a t 
+    b   0 0 0
+    a   0 1 0
+    t   0 0 1
+    z   0 0 0
+    z   0 0 0
+             
+*/
+
 
 var expectedHistory = @"in str1 at 0 char 'c'
 in str2 at 0 char 'b'
